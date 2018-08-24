8a31259e1148ace8d4fa5e049db5271f5e7b9ac1
Nirogu <nirogu@outlook.com>
2018-08-24 08:07:44 -0500

    Update Taller.py

--- Taller.py
+++ Taller.py
@@ -33,4 +33,27 @@ def Vi(A):
 		else:
 			return False
 		
+def Inorder(f):
+	if f.right == None:
+		return f.label,
+	elif f.label == '~':
+		return f.label + Inorder(f.right)
+	else:
+		return "(" + Inorder(f.left) + f.label + Inorder(f.right) + ")"
+
+def StringtoTree(A):
+	stack = []
+	for c in A:
+		if c == "&" or c == "|" or c == "->" or c == "<->":
+			arb = Tree(c, stack[-1], stack[-2])
+			del stack[-1]
+			del stack[-1]
+			stack.append(aux)
+		elif c == '~':
+			arb = Tree(c, None, stack[-1])
+			del stack[-1]
+			stack.append(arb)
+		else:
+			stack.append(Tree(c, None, None))
+
 I = {"p": True, "q": True, "r": False}
