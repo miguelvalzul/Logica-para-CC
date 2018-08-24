e54b9826f9956235dfcc20de4f3e4334954b4f11
miguelvalzul <33555511+miguelvalzul@users.noreply.github.com>
2018-08-24 08:13:34 -0500

    Update Taller.py

--- Taller.py
+++ Taller.py
@@ -4,7 +4,7 @@ class Tree(object):
 		self.right = right
 		self.label = label
 
-def Vi(A):
+def Vi(A,I):
 	if A.right == None:
 		return I[A.label]
 	elif A.label == "~":
@@ -32,6 +32,33 @@ def Vi(A):
 			return True
 		else:
 			return False
+
+def Interps():
+    letrasProposicionales = ['p', 'q', 'r'] # lista con las letras proposicionales
+    interps = [] # lista todas las posibles interpretaciones (diccionarios)
+    aux = {} # primera interpretacion
+    for a in letrasProposicionales:
+        aux[a] = 1 # inicializamos la primera interpretacion con todo verdadero
+    interps.append(aux) # ... y la incluimos en interps
+    for a in letrasProposicionales:
+        interps_aux = [i for i in interps] # lista auxiliar de nuevas interpretaciones
+        for i in interps_aux:
+            aux1 = {} # diccionario auxiliar para crear nueva interpretacion
+            for b in letrasProposicionales:
+                if a == b:
+                    aux1[b] = 1 - i[b] # Cambia el valor de verdad para b
+                else:
+                    aux1[b] = i[b] # ... y mantiene el valor de verdad para las otras letras
+            interps.append(aux1) # Incluye la nueva interpretacion en la lista
+    print 'Interpretaciones: '
+    for i in interps:
+        print i
+def Equival(arb1, arb2):
+    a = True
+    for I in Interps():
+        if Vi(arb1,I) != Vi(arb2,I):
+            a = False
+    return a
 		
 def Inorder(f):
 	if f.right == None:
@@ -57,8 +84,10 @@ def StringtoTree(A):
 			stack.append(Tree(c, None, None))
 	return stack[-1]
 
-I = {"p": True, "q": True, "r": False}
+
 form = raw_input("Ingrese la formula, en notacion polaca inversa: ")
 Arb = StringtoTree(form)
 print ("Usted ha creado el arbol", Inorder(Arb))
-print ("El valor de verdad del arbol es" + Vi(Arb))
+print ("El valor de verdad del arbol es" + Vi(Arb,I))
+
+
