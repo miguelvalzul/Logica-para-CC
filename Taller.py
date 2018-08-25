a5a1caaf6276cf78753c45dcd96e1455eb506689
Nirogu <nirogu@outlook.com>
2018-08-25 14:32:15 -0500

    Update Taller.py

--- Taller.py
+++ Taller.py
@@ -34,60 +34,65 @@ def Vi(A,I):
 			return False
 
 def Interps():
-    letrasProposicionales = ['p', 'q', 'r'] # lista con las letras proposicionales
-    interps = [] # lista todas las posibles interpretaciones (diccionarios)
-    aux = {} # primera interpretacion
-    for a in letrasProposicionales:
-        aux[a] = 1 # inicializamos la primera interpretacion con todo verdadero
-    interps.append(aux) # ... y la incluimos en interps
-    for a in letrasProposicionales:
-        interps_aux = [i for i in interps] # lista auxiliar de nuevas interpretaciones
-        for i in interps_aux:
-            aux1 = {} # diccionario auxiliar para crear nueva interpretacion
-            for b in letrasProposicionales:
-                if a == b:
-                    aux1[b] = 1 - i[b] # Cambia el valor de verdad para b
-                else:
-                    aux1[b] = i[b] # ... y mantiene el valor de verdad para las otras letras
-            interps.append(aux1) # Incluye la nueva interpretacion en la lista
-    print 'Interpretaciones: '
-    for i in interps:
-        print i
-def Equival(arb1, arb2):
-    a = True
-    for I in Interps():
-        if Vi(arb1,I) != Vi(arb2,I):
-            a = False
-    return a
+	letrasProposicionales = ['p', 'q', 'r']
+	interps = []
+	aux = {}
+	for a in letrasProposicionales:
+		aux[a] = True
+	interps.append(aux)
+	for a in letrasProposicionales:
+		interps_aux = [i for i in interps]
+		for i in interps_aux:
+			aux1 = {}
+			for b in letrasProposicionales:
+				if a == b:
+					aux1[b] = not i[b]
+				else:
+					aux1[b] = i[b]
+			interps.append(aux1)
+	return interps
+
+def Equival(arb1, arb2, I):
+	a = True
+	for x in I():
+		if Vi(arb1, x) != Vi(arb2,x):
+			a = False
+			break
+	return a
 		
-def Inorder(f):
-	if f.right == None:
-		return f.label,
-	elif f.label == '~':
-		return f.label + Inorder(f.right)
+def Inorder(A):
+	if A.right == None:
+		return A.label,
+	elif A.label == '~':
+		return A.label + Inorder(A.right)
 	else:
-		return "(" + Inorder(f.left) + f.label + Inorder(f.right) + ")"
+		return "(" + Inorder(A.left) + A.label + Inorder(A.right) + ")"
 
 def StringtoTree(A):
 	stack = []
 	for c in A:
 		if c == "&" or c == "|" or c == "->" or c == "<->":
-			arb = Tree(c, stack[-1], stack[-2])
+			arb = Tree(stack[-1], stack[-2], c)
 			del stack[-1]
 			del stack[-1]
 			stack.append(aux)
 		elif c == '~':
-			arb = Tree(c, None, stack[-1])
+			arb = Tree(None, stack[-1], c)
 			del stack[-1]
 			stack.append(arb)
 		else:
-			stack.append(Tree(c, None, None))
+			stack.append(Tree(None, None, c))
 	return stack[-1]
 
 
-form = raw_input("Ingrese la formula, en notacion polaca inversa: ")
-Arb = StringtoTree(form)
-print ("Usted ha creado el arbol", Inorder(Arb))
-print ("El valor de verdad del arbol es" + Vi(Arb,I))
-
-
+form = raw_input("Ingrese la primera formula, en notacion polaca inversa: ")
+Arb1 = StringtoTree(form)
+print ("Usted ha creado la formula", Inorder(Arb1))
+form = raw_input("Ingrese la segunda formula, en notacion polaca inversa: ")
+Arb2 = StringtoTree(form)
+print ("Usted ha creado la formula", Inorder(Arb2))
+i = Interps()
+if Equival(Arb1, Arb2, i):
+	print "Las dos formulas son equivalentes"
+else:
+	print "Las dos formulas no son equivalentes"
